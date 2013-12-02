f684a68f6d78535a4fe52939844a3fbe699dd542
Jake W <xboxlover360@gmail.com>
2013-12-02 15:40:04 -0600

    liblight: Notification and Capacitive light fix @jbott @MiZrY101

--- liblight/lights.c
+++ liblight/lights.c
@@ -110,6 +110,17 @@ set_light_buttons(struct light_device_t* dev,
     return err;
 }
 
+static int 
+set_light_buttons_from_backlight( int state )
+{
+    int err = 0;
+    int on = is_lit(state);
+    pthread_mutex_lock(&g_lock);
+    err = write_int(BUTTON_FILE, on?255:0);
+    pthread_mutex_unlock(&g_lock);
+    return err;
+}
+
 static int
 set_light_backlight(struct light_device_t* dev,
         struct light_state_t const* state)
@@ -119,6 +130,7 @@ set_light_backlight(struct light_device_t* dev,
     pthread_mutex_lock(&g_lock);
     err = write_int(LCD_FILE, brightness);
     pthread_mutex_unlock(&g_lock);
+    set_light_buttons_from_backlight(state);
     return err;
 }
 
